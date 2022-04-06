783ca89d550a203e5ce5868a326312a0d4510eec
Akshay Nair <phenax5@gmail.com>
2022-04-06 22:43:27 +0530

    init commit

---
+++ default.nix
@@ -0,0 +1,20 @@
+with import <nixpkgs> { };
+mkShell rec {
+  buildInputs = [
+    # Build tools
+    rustup
+    pkg-config
+
+    # Deps
+    libclang
+
+    # Dev
+    nodePackages.nodemon
+    rust-analyzer
+  ];
+  nativeBuildInputs = [ clang ];
+
+  LIBCLANG_PATH = "${libclang.lib}/lib";
+  RUST_SRC_PATH = rust.packages.stable.rustPlatform.rustLibSrc;
+  LD_LIBRARY_PATH = lib.makeLibraryPath (buildInputs ++ nativeBuildInputs);
+}
